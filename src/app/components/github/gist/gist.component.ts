80af1fa103a883876efa76fe98756496f0cda782
Juan Pablo <jplantes@gmail.com>
2020-05-13 08:12:46 -0300

    Fragmentos de código

--- src/app/components/github/gist/gist.component.ts
+++ src/app/components/github/gist/gist.component.ts
@@ -11,6 +11,7 @@ export class GistComponent implements OnInit {
 
   listGist: any = [];
   cantidad: Number = 0;
+  ver: Boolean = false;
 
   constructor(
     private _gist: GistService
@@ -23,6 +24,8 @@ export class GistComponent implements OnInit {
       this.listGist = resp;
 
       this.cantidad = this.listGist.length;
+
+      this.ver = true;
 
     });
 
